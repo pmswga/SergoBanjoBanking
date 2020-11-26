d63c212683cc66430ce86106686d238a4976a6d8
Sergey <pmswga@gmail.com>
2020-11-26 18:33:34 +0300

    Edit welcome page

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Sergo Bango Bancking | Backend'),
+    'name' => env('APP_NAME', 'Sergo Banjo Bancking | Backend'),
 
     /*
     |--------------------------------------------------------------------------
