f07da412afe69cfd6fa684cee1499c863952bd6f
Sergey <pmswga@gmail.com>
2020-12-15 12:01:34 +0300

    some data

--- app/src/main/java/com/sbb/sergobanjobanking/MainActivity.java
+++ app/src/main/java/com/sbb/sergobanjobanking/MainActivity.java
@@ -2,7 +2,9 @@ package com.sbb.sergobanjobanking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+
+    public void onLoginButtonClick(View view)
+    {
+        switch (view.getId())
+        {
+            case R.id.loginButton:
+            {
+                Intent intent = new Intent(this, HomeActivity.class);
+                startActivity(intent);
+            } break;
+        }
+
+    }
 
 }
